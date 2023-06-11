e962a1d530727f90d579bfb0a3fd6b322bb094a5
BotrRobotgnom <user84443@gmail.com>
2023-06-11 12:45:24 +0300

    Create anim_to_minecraft.lua
    v0.1

---
+++ anim_to_minecraft.lua
@@ -0,0 +1,107 @@
+-- Author: BotrRobotgnom
+-- GitHub: https://github.com/BotrRobotgnom/Asesprite-animation-to-Minecraft
+
+if app.activeSprite ~= nil then
+    local dlg = Dialog{title = "AAtoM v0.1"}
+
+    -- Add elements to the dialog window
+    dlg:label{ id="label", text="Select save type:" }
+    dlg:combobox{ id="saveType", option="simple", options={ "simple" } }
+    dlg:newrow()
+
+    dlg:label{ id="fpsLabel", text="Frames per second:" }
+    dlg:entry{ id="fpsEntry", text=tostring(#app.activeSprite.frames) }
+    dlg:newrow()
+
+    -- Function called when the "Save" button is clicked
+    function onSaveButtonClick()
+        local data = dlg.data
+        -- Access the current sprite
+        local spr = app.activeSprite
+
+        -- Get the width and height of the first frame
+        local width = spr.width
+        local height = spr.height
+
+        -- Create a new sprite with the desired width and height
+        local newSpr = Sprite(width, height * #spr.frames, spr.colorMode, spr.pixelRatio)
+        local newLayer = newSpr.layers[1]
+        local newFrame = newSpr.frames[1]
+        local newCel = newSpr:newCel(newLayer, newFrame)
+
+        -- Iterate over each animation frame
+        for i, frame in ipairs(spr.frames) do
+            -- Get the image of the current frame
+            local cel = spr.layers[1]:cel(frame.frameNumber)
+            local image = cel.image
+
+            -- Calculate the position to insert the frame in the new sprite
+            local position = Point(0, height * (i - 1))
+
+            -- Insert the frame image into the new sprite
+            newCel.image:drawImage(image, position)
+        end
+
+        filename = spr.filename:gsub("{filename}", (spr.filename:match("^.*(.+)$")))
+        newSpr:saveAs(filename..".png")
+        newSpr:close()
+
+        framespeed = tonumber(data.fpsEntry)
+
+        -- Create animation data in JSON format
+        local animationData = {
+            animation = {
+                frametime = framespeed, -- Calculate the duration of one frame
+                frames = {}
+            }
+        }
+
+        -- Create JSON string manually
+        local jsonData = '{\n  "animation": {\n'
+        jsonData = jsonData .. '    "frametime": ' .. animationData.animation.frametime .. ',\n'
+        jsonData = jsonData .. '    "frames": [\n'
+
+        -- Check the selected save type
+        if data.saveType == "simple" then
+            -- Add each animation frame to the table
+            for i, frame in ipairs(spr.frames) do
+                local frameData = {
+                    index = i - 1, -- Frame index (starting from 0)
+                }
+                table.insert(animationData.animation.frames, frameData)
+            end
+
+            for i, frameData in ipairs(animationData.animation.frames) do
+                jsonData = jsonData .. '      ' .. frameData.index
+                if i < #animationData.animation.frames then
+                    jsonData = jsonData .. ','
+                end
+                jsonData = jsonData .. '\n'
+            end  
+        end
+
+        jsonData = jsonData .. '    ]\n}\n}\n'
+
+        -- Create the mcmeta file and write the data to it
+        local mcmetaFile = io.open(filename .. ".mcmeta", "w")
+        mcmetaFile:write(jsonData)
+        mcmetaFile:close()
+
+        -- Close the dialog window
+        dlg:close()
+    end
+
+    -- Attach the function to the "Save" button click event
+    dlg:button{ text="Save", onclick=onSaveButtonClick }
+
+    -- Display the dialog window
+    dlg:show()
+else 
+    app.alert {
+        title = "Error",
+        text = "No active sprite found.",
+        buttons = { "OK" }
+    }  
+end
+
+
